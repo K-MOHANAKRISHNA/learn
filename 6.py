60b5ae90fc5e2489efc7c83847a856d5645ee52c
K-MOHANAKRISHNA <85103363+K-MOHANAKRISHNA@users.noreply.github.com>
2023-05-04 16:03:34 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,17 @@
+from numpy import asarray
+from sklearn.preprocessing import OneHotEncoder
+data = asarray([['red',], ['green'], ['blue']])
+print(data)
+encoder = OneHotEncoder(sparse=False)
+onehot = encoder.fit_transform(data)
+print(onehot)
+
+
+'''
+[['red']
+ ['green']
+ ['blue']]
+[[0. 0. 1.]
+ [0. 1. 0.]
+ [1. 0. 0.]]
+ '''
